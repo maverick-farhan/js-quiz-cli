761915891015f45cd85c3fe4dd62730ea61f2051
maverick-farhan <mdfarhanofficial@gmail.com>
2024-08-30 21:22:09 +0530

    dbuged some code

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import figlet from 'figlet';
 import { createSpinner } from 'nanospinner';
 
 let playerName;
-const sleep = (ms = 2000)=>new Promise((r)=>setTimeout(r,ms));
 const total_questions = 4
 let count = 0;
 const answer_lists = [
@@ -16,20 +15,23 @@ const answer_lists = [
   'const',
   'nope, it is not',
   'no...never, what are u taking about?',
-];
-async function title(){
-  const title = 'JS QUIZ';
-  figlet(title,(err,data)=>{
+]
+
+const sleep = (ms = 1000)=>new Promise((r)=>setTimeout(r,ms));
+
+async function gameTitle(){
+  const msg = "JS CLI QUIZ";
+  figlet(msg,(err,data)=>{
   console.log(gradient.pastel.multiline(data));
   });
 }
+
 async function welcome(){
   const rainbowTitle = chalkAnimation.rainbow(
     'Who Wants To Be A JS Wizard? \n'
   );
 
   await sleep();
-  rainbowTitle.start();
   rainbowTitle.stop();
 }
 
@@ -44,7 +46,6 @@ async function askName(){
   });
   playerName = answers.player_name;
   console.log(`\n Welcome ${playerName} to the CLI based QUIZ game.`);
-
 }
 
 async function ques1(){
@@ -109,15 +110,19 @@ async function handleAnswer(isCorrect){
     spinner.success({text:`Nice work ${playerName}. That's a correct answer`});
   }
   else{
-  count-=1;
+    if(count<0){
+      count = 0;
+    }
     spinner.error({text:`💀💀💀💀 Game Over,you are wrong ${playerName}`});
     winner("Good Work");
+  console.log(
+    chalk.yellow( `${playerName}! good try ${count}/${total_questions} points`));
     process.exit(1);
   }
 }
 async function winner(callback){
   console.clear();
-  const msg = `${callback} ${playerName}`;
+  const msg = `${callback}`;
   figlet(msg,(err,data)=>{
   console.log(gradient.pastel.multiline(data));
   });
@@ -126,9 +131,7 @@ async function result(){
   console.log(
     chalk.yellow( `${playerName}! you won with ${count}/${total_questions} points`));
 }
-
-
-await title();
+await gameTitle();
 await welcome();
 await askName();
 await ques1();
